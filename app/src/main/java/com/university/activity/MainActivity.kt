30fc416441490230b580331a561840e8f32302eb
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-05-05 19:23:38 +0300

    move MainActivity to activity/

---
+++ app/src/main/java/com/university/activity/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.university.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.university.R
+import com.university.listing.presentation.view.UniversityListFragment
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
+
+    @Inject
+    lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return fragmentInjector
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        navigateToUniversityListingScreen()
+    }
+
+    private fun navigateToUniversityListingScreen() {
+        supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.fl_screen_container,
+                UniversityListFragment(),
+                UniversityListFragment::class.simpleName
+            ).commit()
+    }
+}
