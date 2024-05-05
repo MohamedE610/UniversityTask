a291587a69b4a7337205490e5ba3d565b4aab5a6
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-05-05 20:09:50 +0300

    implement UniversityListViewModel in presentation layer

---
+++ listing/src/main/java/com/university/listing/presentation/viewmodel/UniversityListViewModel.kt
@@ -0,0 +1,46 @@
+package com.university.listing.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.university.core.exception.AppException
+import com.university.core.exception.toAppException
+import com.university.entity.University
+import com.university.listing.domain.GetUniversityListUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+class UniversityListViewModel @Inject constructor(
+    private val getUniversityListUseCase: GetUniversityListUseCase
+) : ViewModel() {
+
+    private val _screenState: MutableStateFlow<UniversityListState> by lazy {
+        MutableStateFlow(UniversityListState.Initial)
+    }
+
+    internal val screenState: StateFlow<UniversityListState> = _screenState
+
+    init {
+        getUniversityList()
+    }
+
+    fun getUniversityList() {
+        _screenState.value = UniversityListState.Loading
+        getUniversityListUseCase()
+            .onEach { _screenState.value = UniversityListState.Success(it) }
+            .onEach { Log.d("asdasd", "list size: ${it.size}") }
+            .catch { _screenState.value = UniversityListState.Error(it.toAppException()) }
+            .launchIn(viewModelScope)
+    }
+}
+
+sealed class UniversityListState {
+    data object Initial : UniversityListState()
+    data object Loading : UniversityListState()
+    data class Success(val data: List<University>) : UniversityListState()
+    data class Error(val e: AppException) : UniversityListState()
+}
