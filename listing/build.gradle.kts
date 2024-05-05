0e3f37db0778a02aa7d7f2c70ba74fc1d22e9af8
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-05-05 16:17:58 +0300

    add gradle dependencies to listing module

--- listing/build.gradle.kts
+++ listing/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.orgJetbrainsKotlinkapt)
 }
 
 android {
@@ -33,11 +34,22 @@ android {
 }
 
 dependencies {
+    implementation(project(":core"))
+    implementation(project(":entity"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //mockito
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.inline)
+
+    //dagger2
+    kapt(libs.dagger.compiler)
+    kapt(libs.dagger.android.processor)
 }
