c23cbac732b8bd7c25d9ef8560ded919bb52cd6d
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-05-05 04:14:25 +0300

    create RoomConstants in core/datasource/local/room/constant

---
+++ core/src/main/java/com/university/core/datasource/local/room/constant/RoomConstants.kt
@@ -0,0 +1,7 @@
+package com.university.core.datasource.local.room.constant
+
+object RoomConstants {
+    const val TABLE_NAME = "university_table"
+    const val DB_VERSION = 1
+    const val DB_NAME = "university_db"
+}
