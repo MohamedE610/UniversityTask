a102f9bf0f0d584f17e2888c2558e9d164ff277a
Mohamed Mostafa <mohamedm9595@gmail.com>
2024-05-05 04:13:41 +0300

    create UniversityEntity in core/datasource/local/room/entity

---
+++ core/src/main/java/com/university/core/datasource/local/room/entity/UniversityEntity.kt
@@ -0,0 +1,29 @@
+package com.university.core.datasource.local.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.university.core.datasource.local.room.constant.RoomConstants
+import com.university.core.datasource.local.room.converters.DBConverters
+
+@Entity(tableName = RoomConstants.TABLE_NAME)
+data class UniversityEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: String,
+    @ColumnInfo(name = "alphaTwoCode")
+    val alphaTwoCode: String,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "country")
+    val country: String,
+    @TypeConverters(DBConverters::class)
+    @ColumnInfo(name = "domains")
+    val domains: List<String>,
+    @TypeConverters(DBConverters::class)
+    @ColumnInfo(name = "webPages")
+    val webPages: List<String>,
+    @ColumnInfo(name = "stateProvince")
+    val stateProvince: String
+)
